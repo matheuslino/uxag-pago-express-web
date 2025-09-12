c570186879bba674fb7c9c99f4a56ebb728b10e0
GiovaneMS <giovane.m.s.cc@gmail.com>
2025-09-12 08:53:48 -0300

    Criação e Edição Wallets

--- src/app/features/wallets/edit/edit.ts
+++ src/app/features/wallets/edit/edit.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-edit',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule 
+  ],
   templateUrl: './edit.html',
   styleUrl: './edit.scss'
 })
 export class Edit {
+
+  public razaoSocial: string = '123 Milhas';
+  public apelido: string = 'Default';
+  public numeroCarteira: string = '11897';
+  public valor: string = '0,00';
 
 }
--- src/app/features/wallets/new/new.ts
+++ src/app/features/wallets/new/new.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'; 
 
 @Component({
   selector: 'app-new',
-  imports: [],
+  standalone: true, 
+  imports: [
+    CommonModule,
+    FormsModule 
+  ],
   templateUrl: './new.html',
   styleUrl: './new.scss'
 })
 export class New {
+
+  public razaoSocial: string = ''; 
+  public apelido: string = '';
 
 }
