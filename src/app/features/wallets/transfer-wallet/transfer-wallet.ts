54ed8f4a0ce19dd57c9fd39e94f84b26231cce95
GiovaneMS <giovane.m.s.cc@gmail.com>
2025-09-12 09:45:39 -0300

    Transferência entre contas

--- src/app/features/wallets/transfer-wallet/transfer-wallet.ts
+++ src/app/features/wallets/transfer-wallet/transfer-wallet.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-transfer-wallet',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './transfer-wallet.html',
   styleUrl: './transfer-wallet.scss'
 })
