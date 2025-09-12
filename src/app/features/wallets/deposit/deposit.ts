e8df0593833a91c604169e289020b8aad2d4f36a
GiovaneMS <giovane.m.s.cc@gmail.com>
2025-09-12 09:44:55 -0300

    Depósito

--- src/app/features/wallets/deposit/deposit.ts
+++ src/app/features/wallets/deposit/deposit.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-deposit',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './deposit.html',
   styleUrl: './deposit.scss'
 })
