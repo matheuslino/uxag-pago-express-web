5a8847073d2141c2e76435def2f049e6492db265
Kelvyn Faria <kelvyn_faria@yahoo.com.br>
2025-09-25 11:55:20 -0300

    Ajustes finais de QA

--- src/app/features/wallets/new/new.ts
+++ src/app/features/wallets/new/new.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms'; 
+import { CommonModule, Location } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'; 
 import { Router } from '@angular/router';
 import { WalletSidebar } from '../../../shared/components/wallet-sidebar/wallet-sidebar';
 import { Header } from '../../../shared/components/header/header';
 import { HeaderTitle } from '../../../shared/components/header-title/header-title';
+import { FooterInfo } from '../../../shared/components/footer-info/footer-info';
 
 @Component({
   selector: 'app-new',
@@ -13,15 +14,21 @@ import { HeaderTitle } from '../../../shared/components/header-title/header-titl
     WalletSidebar,
     HeaderTitle,
     CommonModule,
-    FormsModule 
+    FormsModule,
+    ReactiveFormsModule,
+    FooterInfo,
   ],
   templateUrl: './new.html',
   styleUrl: './new.scss'
 })
 export class New {
 
+  public sendLabel: string = 'Salvar';
   public razaoSocial: string = ''; 
   public apelido: string = '';
+  public footerInformation: string = '#1132 Barbearia Orizon';
+  public footerContext: string = '123 Milhas';
+  public footerLabel: string = 'Carteira oficial';
 
   public headerInformation = {
     pageTitle: 'Wallets',
@@ -41,6 +48,17 @@ export class New {
 
   public menuAbertoIndex: number | null = null;
 
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router,
+    private location: Location
+  ) { }
+
+  onCancel(): void {
+    this.location.back();
+  }
+
+  onSubmit(): void {
+    this.location.back();
+  }
 
 }
